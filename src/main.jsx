71487d324333499379ccf3afa8000634484e54c6
Durga1234-cloud <puhandurgasankarp@gmail.com>
2025-03-13 22:09:05 +0530

    Final

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ import EditPost from "./pages/EditPost";
 
 import Post from "./pages/Post";
 
-import AllPosts from './pages/AllPosts.jsx'
+import AllPosts from "./pages/Allposts";
 
 const router = createBrowserRouter([
   {
